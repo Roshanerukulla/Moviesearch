e88f0014ec7fd0846bc1969ca50863c2a2bb2920
Roshanerukulla <roshan8019924268@gmail.com>
2024-02-14 19:33:51 -0500

    dd

--- marvel/search.php
+++ marvel/search.php
@@ -5,11 +5,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include 'dbconnection.php';
     
     $movie_name = $_POST["Movie"];
-    $year = $_POST["ReleaseYear"];
+    $year = $_POST["Release Year"];
     $studio = $_POST["Ownership"];
     $director = $_POST["Director"];
     
-    $sql = "SELECT Movie FROM Marvel WHERE $movie_name LIKE '%$movie_name'";
+    $sql = "SELECT Movie FROM db WHERE $movie_name LIKE '%$movie_name'";
     $conn->query($sql);
 
     exit();
